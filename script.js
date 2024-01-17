2b5df4e061a631eb24e24b75c9f89bdfa0fb92fc
MostLeVert <axeljalonen@gmail.com>
2024-01-17 13:38:28 -0800

    Update play button label and add new shape for Tetris game

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const cellContainer = document.getElementById("cell-container");
 const playBtn = document.getElementById("play-btn");
 
-let boardState = {};
+let boardState = [];
+// board sate has form:
+// [
+//   {id: 0, color: "black", active: false},
+//   ...
+// ]
+// Where id is the cell number, and active represents if the cell is part of the active piece
+
 let activePiece;
+
 let shapes = [
   {
     name: "T",
     color: "purple",
-    shape: [1, (1 + 10), (2 + 10), (3 + 10)],
-  }
-]
+    shape: [1, 10, 1 + 10, 2 + 10],
+  },
+  {
+    name: "L",
+    color: "orange",
+    shape: [0, 10, 20, 21],
+  },
+];
 
 function initPageState() {
   for (let i = 0; i < 200; i++) {
-    boardState[i] = "black";
+    boardState.push({ id: i, color: "black", active: false });
     const cell = document.createElement("div");
     cell.className = "grid";
     cell.id = i;
@@ -21,7 +34,6 @@ function initPageState() {
   }
 }
 initPageState();
-console.log(boardState);
 
 // document.addEventListener("keydown", handleKeyDown);
 playBtn.addEventListener("click", wrapToggle);
@@ -33,27 +45,46 @@ function wrapToggle() {
     if (started) {
       clearInterval(intervalId);
     } else {
-      intervalId = setInterval(renderBoard, 1000);
+      intervalId = setInterval(computePiece, 100);
     }
-    playBtn.textContent = started ? "Pause" : "Play";
     started = !started;
   }
   toggleGame();
 }
 
+function clearBoard() {
+  for (let i = 0; i < 200; i++) {
+    document.getElementById(i).style.background = "black";
+  }
+}
+
 function renderBoard() {
+  clearBoard();
   for (let key in boardState) {
     document.getElementById(key).style.background = boardState[key];
   }
 }
 
 function computePiece() {
-  updateBoardState(shapes[0]);
+  if (!activePiece) {
+    const randomPiece = shapes[Math.floor(Math.random() * shapes.length)];
+    activePiece = randomPiece;
+    const pieceNewPos = updateBoardState(activePiece);
+    renderBoard(pieceNewPos);
+  } else {
+    const pieceNewPos = updateBoardState(activePiece);
+    if (pieceNewPos.shape[3] >= 190) {
+      activePiece = null;
+      renderBoard(pieceNewPos);
+      return;
+    }
+  }
   renderBoard();
 }
 
 function updateBoardState(piece) {
   for (let i = 0; i < piece.shape.length; i++) {
+    piece.shape[i] += 10;
     boardState[piece.shape[i]] = piece.color;
   }
   return piece;
